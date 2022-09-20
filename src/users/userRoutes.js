d54c3b42e1b079fe84515c6abe2aa7d65621f2aa
r b <brittonr@hotmail.co.uk>
2022-09-20 14:22:29 +0100

    finished crud operations

--- src/users/userRoutes.js
+++ src/users/userRoutes.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
-const { addUser, login, listUsers, removeUser, updateUser } = require("./userControllers");
+const { addUser, login, listUsers, removeUser, updateUserInfo, updatePassword,updateCurrentWeight } = require("./userControllers");
 const { hashPassword, tokenCheck } = require("../middleware");
 
 const userRouter = Router();
 
 userRouter.post("/user/signup", [hashPassword], addUser);
 userRouter.post("/user/login", login);
-userRouter.get("/user/list", listUsers); //<- originally had tokencheck middleware
-userRouter.post("/user/remove", removeUser); //<- originally had tokencheck middleware
-userRouter.patch("/user/update", [hashPassword], updateUser); //<- originally had tokencheck middleware
+userRouter.get("/user/list", listUsers); //<- later needs tokencheck middleware
+userRouter.post("/user/remove", removeUser); //<- later needs tokencheck middleware
+userRouter.patch("/user/updateUserInfo", updateUserInfo); //<- later needs tokencheck middleware
+userRouter.patch("/user/updatePassword", [hashPassword], updatePassword); //<- later needs tokencheck middleware
+userRouter.patch("/user/updateCurrentWeight", updateCurrentWeight); //<- later needs tokencheck middleware
 
 
 module.exports = userRouter;
